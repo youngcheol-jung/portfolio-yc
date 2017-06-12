50c4bb22e26cfe34a2700d77db2b38a079256063
youngcheol-jung <iamjungyoungcheol@gmail.com>
2017-06-12 12:20:52 +0900

    그런트셋팅

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    serve: {
+        options: {
+            port: 9000
+        }
+    },
+    less: {
+        min: {
+        options: {
+          paths: ['assets/css'],
+          compress:true
+        },
+        files: {
+          './assets/css/style.css': './assets/less/import.less'
+        }
+    },
+    dev: {
+        options: {
+          paths: ['assets/css'],
+          dumpLineNumbers:"comments"
+        },
+        files: {
+          './assets/css/style.dev.css': './assets/less/import.less'
+        }
+      }
+  },
+  watch: {
+      scripts: {
+        files: ['./assets/less/*.less'],
+        tasks: ['less'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+});
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-serve');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['serve']);
+
+};
