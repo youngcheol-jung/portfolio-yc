32b5e8ec36fe4d92f9b56babaf90ddead5353eb7
youngcheol-jung <iamjungyoungcheol@gmail.com>
2017-06-29 12:33:55 +0900

    111

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,12 +15,12 @@ module.exports = function(grunt) {
     // grunt-includes : html 인크루드 사용
     includes:{
         dist:{
-            cwd:'src/views/',
+            cwd:'html/',
             src:['**/*.html'],
-            dest:'dist/views/',
+            dest:'dist/',
             options:{
                 flatten:true,
-                includePath:'src/views/includes/'
+                includePath:'html/include/'
             }
         }
     },
